b786b9d3d845b2bd12f4fdfbd67f2adf16171827
lerashemelinina <35759144+lerashemelinina@users.noreply.github.com>
2018-06-25 22:47:50 +0300

    Add files via upload

---
+++ linqHW/customer.cs
@@ -0,0 +1,23 @@
+﻿namespace linq_homework
+{
+    class Customer
+    {
+        public string CustomerId { get; set; }
+        public string CompanyName { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; } 
+        public string Country { get; set; }
+
+        public Customer(string customerId, string companyName, string adress, string city, string region, string country)
+        {
+            CustomerId = customerId;
+            CompanyName = companyName;
+            Adress = adress;
+            City = city;
+            Region = region;
+            Country = country;
+        }
+
+    }
+}
